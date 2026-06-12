2b3246777c72eed0aee35f8234b3167dee1fcb35
ellietngo <engo@metaphase.tech>
2026-06-12 13:50:01 -0400

    Fix unused React import

--- src/pages/VerifyPage.tsx
+++ src/pages/VerifyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Shield, ArrowLeft, ChevronRight } from 'lucide-react'
 
